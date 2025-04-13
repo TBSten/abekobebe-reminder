c3a5ff7123182a9ee43c9b38bc23b5e16a5fe870
TBSten <programmingCafeteria@gmail.com>
2025-04-13 23:40:08 +0900

    Discord Webhookメッセージの生成ロジックを改善し、CRON_SECRETの取得方法を修正。package-lock.jsonにSWC関連の新しいモジュールを追加。

--- app/handle-cron/route.ts
+++ app/handle-cron/route.ts
@@ -1,10 +1,21 @@
 import { NextRequest } from "next/server";
 
+const CRON_SECRET = process.env.CRON_SECRET as string
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL as string
+
+const message = ()=> {
+    const now = new Date()
+    const after7Days = new Date(now.valueOf())
+    after7Days.setDate(after7Days.getDate() + 7)
+    return `
+<@チームF>
+${now.getMonth() + 1}/${now.getDate()}~${after7Days.getMonth() + 1}/${after7Days.getDate() + 1} の ペアプロ/レビュー会
+`.trim()
+}
 
 export const GET = async (request: NextRequest) => {
     const authHeader = request.headers.get('authorization');
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    if (authHeader !== `Bearer ${CRON_SECRET}`) {
       return new Response('Unauthorized', {
         status: 401,
       });
@@ -17,7 +28,7 @@ export const GET = async (request: NextRequest) => {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            "content": `${now.getMonth() + 1}/${now.getDate()}~${now.getMonth() + 1}/${now.getDate() + 1 + 7} の ペアプロ/レビュー会 @チームF`,
+            "content": message(),
             "allowed_mentions": {
                 "parse": ["roles"],
             },
